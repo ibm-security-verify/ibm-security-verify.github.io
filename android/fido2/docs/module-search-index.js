ab5fa3e6dc0eb81cc51279320c2bd95490d6438f
Carsten Hagemann <57122841+chagemann@users.noreply.github.com>
2024-03-07 21:39:29 +1000

    Update Android documentation for SDK v3.0.1 (#9)
    * Add FIDO2 SDK documentation
    
    * Update from https://github.com/ibm-security-verify/verify-sdk-android/commit/4c96cc62505f39a64bc95a5651f56745a0653f69
    
    * Update from https://github.com/ibm-security-verify/verify-sdk-android/commit/fd09a6d0c6586059e5d1cd00451b32dfbfe6e36a
    
    * Add Android tiles

---
+++ android/fido2/docs/module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":"IBM Security Verify FIDO2(TM) SDK for Android","url":"index.html"}]
---
+++ android/fido2/docs/package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"com.ibm.security.verifysdk.fido2","url":"com/ibm/security/verifysdk/fido2/package-summary.html"}, {"l":"com.ibm.security.verifysdk.fido2.model","url":"com/ibm/security/verifysdk/fido2/model/package-summary.html"}, {"l":"All packages","url":"index.html"}]
